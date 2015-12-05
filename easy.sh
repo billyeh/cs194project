bf81b8ff2d4f396ee583d516fb43d7cdd9673aa5
billyeh <billwyeh@gmail.com>
2015-12-05 12:31:03 -0800

    make easy better

--- easy.sh
+++ easy.sh
@@ -1,17 +1,24 @@
 echo "*** Featurizing ***"
-python featurize.py data/review_short_train.json train data/review_short_test.json test
+time python featurize.py data/review_short_train.json train data/review_short_test.json test
 
-echo "*** Cross validation ***"
-cd libsvm-3.20/tools
-time python grid.py ../../train ../../test | tail -n1 | tee ../../params
-cd ../..
-
-echo "*** Training and predicting ***"
 C=$(cat params | cut -d' ' -f1)
 g=$(cat params | cut -d' ' -f2)
+if [[ $* == *--cross-validate* ]]
+then
+  echo "*** Cross validation ***"
+  cd libsvm-3.20/tools
+  time python grid.py ../../train ../../test | tail -n1 > ../../params
+  cd ../..
+else
+  echo "*** Skipping cross validation ***"
+  C="32.0"
+  G="0.03125"
+fi
 
 echo "C=$C"
 echo "g=$g"
 
-./libsvm-3.20/svm-train -c $C -g $g train
-./libsvm-3.20/svm-predict test train.model out
+echo "*** Training ***"
+time ./libsvm-3.20/svm-train -c $C -g $g train
+echo "*** Predicting ***"
+time ./libsvm-3.20/svm-predict test train.model out
