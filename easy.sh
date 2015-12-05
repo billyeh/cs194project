d91e8a2e59df15be3b07e9cf9d61385a06364b6a
billyeh <billwyeh@gmail.com>
2015-12-05 12:38:46 -0800

    get C and g

--- easy.sh
+++ easy.sh
@@ -1,14 +1,14 @@
 echo "*** Featurizing ***"
 time python featurize.py data/review_short_train.json train data/review_short_test.json test
 
-C=$(cat params | cut -d' ' -f1)
-g=$(cat params | cut -d' ' -f2)
 if [[ $* == *--cross-validate* ]]
 then
   echo "*** Cross validation ***"
   cd libsvm-3.20/tools
   time python grid.py ../../train ../../test | tail -n1 > ../../params
   cd ../..
+  C=$(cat params | cut -d' ' -f1)
+  g=$(cat params | cut -d' ' -f2)
 else
   echo "*** Skipping cross validation ***"
   C="32.0"
